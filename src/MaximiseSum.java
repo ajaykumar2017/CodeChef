4c51c3f02df40e27c8ef3d7789f09a4fe06f236f
AJAY KUMAR <43679751+ajaykumar2017@users.noreply.github.com>
2019-06-19 22:26:30 +0530

    Update MaximiseSum.java

--- src/MaximiseSum.java
+++ src/MaximiseSum.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class MaximiseSum {
     public static void main(String args[]){
         Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
         
     }
 }
