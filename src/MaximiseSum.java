3190a8337a29d647694812ac2361129dc76aba97
Ajay Kumar <ajaykumar201761@gmail.com>
2019-06-20 15:57:31 +0530

    start ffty

--- src/MaximiseSum.java
+++ src/MaximiseSum.java
@@ -1,36 +1,45 @@
 import java.util.*;
 import java.lang.*;
+/*
+*#include <bits/stdc++.h>
+using namespace std;
+long long int A[1000010],z;
+int main(){
+    int T,N,i;
+    cin >> T;
+    while(T--){
+        cin >> N;
+        i=N;
+        while(i--) cin >> A[i];
+        sort(A,A+N);
+        z=0;
+        for(i=0;i<N;i++) z+=abs(A[N/2]-A[i]);
+        cout << z << endl;
+    }
+}
+*
+*
+ */
 public class MaximiseSum {
     public static void main(String args[]){
         Scanner s=new Scanner(System.in);
         int T=s.nextInt();
         s.nextLine();
-        int[] N=new int[T];
-        Integer[] A= new Integer[0];
+        int N;
         for (int i=0;i<T;i++){
-            N[i]=s.nextInt();
-            for(int j=0;j<N[i];j++){
-                A[i]=s.nextInt();
+            N=s.nextInt();
+            s.nextLine();
+            int[] A= new int[N];
+            for(int j=0;j<N;j++){
+                A[j]=s.nextInt();
             }
+            Arrays.sort(A);
+            int z=0;
+            for(int k=0;k<N;k++)
+                z+=Math.abs(A[N/2]-A[k]);
+            System.out.println(z);
+
         }
-        List<Integer> l = Arrays.asList(A);
-        int min=0;
-        for (int k=0;k<T;k++)
-        {
-            for (int i=0;i<N[k];i++)
-            {
-                Collections.shuffle(l);
-                min=Math.abs(l.get(0)-l.get(1))+Math.abs(l.get(2)-l.get(3));
-                for (int j=1;j<N[k];j++)
-                {
-                    Collections.shuffle(l);
-                    int min1=Math.abs(l.get(0)-l.get(1))+Math.abs(l.get(2)-l.get(3));
-                    if (min<min1)
-                        min=min1;
-                }
-            }
-        }
-        System.out.println(min);
         
     }
 }
